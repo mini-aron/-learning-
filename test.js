081d31ce1a24476a4071aaf172968b98e45dfe01
우엥 <105274015+rnldudns@users.noreply.github.com>
2022-07-31 23:27:51 +0900

    javascript test
    Start learning JavaScript!😳

---
+++ test.js
@@ -0,0 +1,12 @@
+
+const Name = prompt("이름을 입력하세요.");
+alert("환영합니다, ${Name} 님");
+const gender ='F';
+let name2 ='jone';
+const isAdult = 'true';
+
+if(gender === 'M'&& (name2 == 'Mike'|| isAdult)){
+    console.log('통과');
+}else{
+    console.log('돌아가.');
+}
